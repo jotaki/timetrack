29208205d52b05c54de26f1c2b81e2ed62bbf0c8
Joseph T. Kinsella <zer0python@gmail.com>
2019-07-20 03:22:07 -0400

    sql helper functions

---
+++ src/bash/sql.sh
@@ -0,0 +1,23 @@
+###
+# sql.sh: wrapper code for sql
+
+sqlblock(){ $SQL_ENGINE $SQL_PARAMS "$TIMETRACK_DB_PATH" $* 2>/dev/null;}
+
+initdb() {
+    sqlblock <<- EOF 2>/dev/null
+<SQL.INIT>
+EOF
+}
+
+trackit() {
+    sqlblock <<- EOF 2>/dev/null
+        insert into timekeep (username, projectname, activity) values
+            ('$1', '$2', '$3');
+EOF
+}
+
+showreport() {
+    sqlblock -column -header "select * from timereport;";
+}
+
+sqlsanitize(){ [ ! -z "$1" ] && sed s:\':\\\\\':g <<< "$1";}
